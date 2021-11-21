a407dbadc5f3e5e7048dba0331d91e19df9daf07
Aditya Jagtap <adityavj2010@gmail.com>
2021-11-20 20:32:53 -0500

    Added segeration

--- contracts/UrFabToken.sol
+++ contracts/UrFabToken.sol
@@ -4,7 +4,70 @@ import "./ERC20.sol";
 import "./Owned.sol";
 import "./SafeMath.sol";
 
-contract UrFabToken is ERC20, Owned, SafeMath {
+contract MiscFunctionalities is SafeMath, Owned {
+    uint256 productCounter = 1;
+
+    struct review {
+        address addr;
+        string comment;
+        uint256 rating;
+    }
+
+    struct product {
+        uint256 uniqueRegNumer;
+        uint256 productCost;
+        string productName;
+        uint256 productCount;
+        address manufacturer;
+        review[] reviews;
+    }
+    mapping(uint256 => product) public products;
+
+    function createProduct(
+        string memory productName,
+        uint256 productCost,
+        uint256 productCount
+    ) public returns (uint256) {
+        productCounter++;
+        uint256 productId = productCounter;
+        products[productId].productCost = productCost;
+        products[productId].productName = productName;
+        products[productId].manufacturer = msg.sender;
+        products[productId].productCount = productCount;
+        return productId;
+    }
+
+    function updateProductCount(uint256 productId, uint256 productCount)
+        public
+        returns (bool)
+    {
+        require(products[productId].manufacturer == msg.sender);
+        products[productId].productCount = safeAdd(products[productId].productCount,productCount);
+        return true;
+    }
+
+    function getProduct(uint256 productId) public view returns (string memory,uint256,address,uint256) {
+        return (
+            products[productId].productName,            
+            products[productId].productCost,
+            products[productId].manufacturer,
+            products[productId].productCount
+        );
+    }
+
+    struct participant {
+        string userName;
+        address addr;
+        uint256 role;
+    }
+
+    mapping(uint256 => participant) public participants;
+    event ProductCreated(address indexed from, address indexed to, uint tokens);
+    event ProductUpdated(address indexed tokenOwner, address indexed spender, uint tokens);
+
+}
+
+contract UrFabToken is ERC20, MiscFunctionalities {
     string public symbol;
     string public name;
     uint8 public decimals;
@@ -40,6 +103,17 @@ contract UrFabToken is ERC20, Owned, SafeMath {
         balance = balances[tokenOwner];
     }
 
+    function approve(address spender, uint256 tokens)
+        public
+        virtual
+        override
+        returns (bool success)
+    {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        success = true;
+    }
+
     function allowance(address tokenOwner, address spender)
         public
         view
@@ -62,26 +136,15 @@ contract UrFabToken is ERC20, Owned, SafeMath {
         success = true;
     }
 
-    function approve(address spender, uint256 tokens)
-        public
-        virtual
-        override
-        returns (bool success)
-    {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        success = true;
-    }
-
     function transferFrom(
         address from,
         address to,
         uint256 tokens
     ) public virtual override returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens); 
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(from, to, tokens);
-        success =true;
+        success = true;
     }
 }
--- test/urfab.js
+++ test/urfab.js
@@ -6,30 +6,105 @@ const urfab = artifacts.require("UrFabToken");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("URFAB", async function (accounts) {
-  const ownerAddress = accounts[0]
+  const ownerAddress = accounts[0];
+  const user1 = accounts[1];
+  console.log("Owner", ownerAddress);
   it("Verify owner", async function () {
     let urfabInstance = await urfab.deployed();
     const address = await urfabInstance.owner.call();
-    return assert.equal(ownerAddress,address)
+    return assert.equal(ownerAddress, address);
   });
 
   it("Check initial balance", async function () {
     let urfabInstance = await urfab.deployed();
     const balance = await urfabInstance.totalSupply();
-    // console.log('accounts',accounts); 
-    return assert.equal(balance.toNumber(),100000000);
+    // console.log('accounts',accounts);
+    return assert.equal(balance.toNumber(), 100000000);
   });
 
   it("Check Token Symbol", async function () {
     let urfabInstance = await urfab.deployed();
     const symbol = await urfabInstance.symbol();
-    return assert.equal(symbol,"URFAB");
+    return assert.equal(symbol, "URFAB");
   });
 
   it("Check decimals", async function () {
     let urfabInstance = await urfab.deployed();
     const decimals = await urfabInstance.decimals();
-    return assert.equal(decimals,18);
+    return assert.equal(decimals, 18);
   });
 
+  it("Verify owner balance", async function () {
+    let urfabInstance = await urfab.deployed();
+    let ownerBalance = (
+      await urfabInstance.balanceOf.call(ownerAddress)
+    ).toNumber();
+    return assert.equal(ownerBalance, 100000000);
+  });
+
+  it("Verify balance of user 1", async function () {
+    let urfabInstance = await urfab.deployed();
+    let balanceUser1 = (await urfabInstance.balanceOf.call(user1)).toNumber();
+    return assert.equal(balanceUser1, 0);
+  });
+
+  it("Make transaction and verify balance", async function () {
+    let urfabInstance = await urfab.deployed();
+    await urfabInstance.transfer(user1, 100, { from: ownerAddress });
+    const balanceUser1New = await urfabInstance.balanceOf.call(user1);
+    const balanceOwnerNew = await urfabInstance.balanceOf.call(ownerAddress);
+    assert.equal(100000000 - 100, balanceOwnerNew.toNumber());
+    return assert.equal(100, balanceUser1New.toNumber());
+  });
+
+  it("Transfer from user1 to owner with transfer function", async function () {
+    let urfabInstance = await urfab.deployed();
+    await urfabInstance.transfer(ownerAddress, 100, { from: user1 });
+    const balanceUser1New = await urfabInstance.balanceOf.call(user1);
+    const balanceOwnerNew = await urfabInstance.balanceOf.call(ownerAddress);
+    assert.equal(100000000, balanceOwnerNew.toNumber());
+    return assert.equal(0, balanceUser1New.toNumber());
+  });
+
+  // it("Verify approval of allowance", async function () {
+  //   let urfabInstance = await urfab.deployed();
+  //   const flag = await urfabInstance.approve.call(user1,100,{from:ownerAddress});
+  //   return assert(flag);
+  // });
+
+  // it("Verify allowance", async function () {
+  //   let urfabInstance = await urfab.deployed();
+  //   const allowance = await urfabInstance.allowance.call(user1,ownerAddress);
+  //   console.log('allowance',allowance.toNumber())
+  //   return assert.equal(allowance.toNumber(),100);
+  // });
+
+  it("Verify product creation", async function () {
+    let urfabInstance = await urfab.deployed();
+    // string memory productName,
+    // uint256 productCost,
+    // uint256 productCount
+    const productName = "Food",
+      productCost = 10,
+      productCount = 100;
+    let productId = await urfabInstance.createProduct.call(
+      productName,
+      productCost,
+      productCount
+    );
+    console.log(productId.toNumber())
+    productId = await urfabInstance.createProduct.call(
+      productName,
+      productCost,
+      productCount
+    );
+    console.log(productId.toNumber())
+    productId = await urfabInstance.createProduct.call(
+      productName,
+      productCost,
+      productCount
+    );
+    console.log(productId.toNumber())
+
+  });
 });
