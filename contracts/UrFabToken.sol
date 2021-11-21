6ce6e594066d8e92bb1cb7b88cbc53aa4f66cb07
EvaPradhan1 <pradhaneva94@gmail.com>
2021-11-21 13:46:50 -0500

    after create product
    everything after create product except for db

--- contracts/UrFabToken.sol
+++ contracts/UrFabToken.sol
@@ -5,65 +5,148 @@ import "./Owned.sol";
 import "./SafeMath.sol";
 
 contract MiscFunctionalities is SafeMath, Owned {
-    uint256 productCounter = 1;
+    enum Roles {Inactive, Active}
+    enum productState {Neutral, Requested, Sold}
 
-    struct review {
+    struct participant {
+        uint256 userCodeName;
         address addr;
-        string comment;
-        uint256 rating;
+        Roles role;
+        bool isRegistered;
     }
+
+    uint256 participantCounter = 0;
+
+    mapping(uint256 => participant) public participants;
+    mapping (address=>uint) membership;
+    
+    modifier onlyRegisteredParticipant()
+    { require(membership[msg.sender] == 1);
+        _;
+    }
+
+    uint256 productCounter = 0;
+
+    uint256 productReviewCounter = 0;
+
+    struct productReview {
+        address reviewer;
+        uint256 parameter_1_rating;
+        uint256 parameter_2_rating;
+        uint256 parameter_3_rating;
+        uint256 overallRating;
+        uint256 productReviewNumber;
+    }
+
+    // ProductId to Reviews mapping
+    mapping(uint256 => review) public productReviews;
 
     struct product {
         uint256 uniqueRegNumer;
         uint256 productCost;
-        string productName;
+        uint256 productCode;
         uint256 productCount;
         address manufacturer;
+        address currentOwner;
+
+        productState status;
+        address buyer;
+        uint256 hashOfDetails;
+
         review[] reviews;
     }
-    mapping(uint256 => product) public products;
 
     function createProduct(
-        string memory productName,
+        uint256 memory productCode,
         uint256 productCost,
-        uint256 productCount
-    ) public returns (uint256) {
+        uint256 productCount,
+        uint256 participantId
+        
+    ) public onlyRegisteredParticipant() returns (uint256) {
         productCounter++;
         uint256 productId = productCounter;
         products[productId].productCost = productCost;
-        products[productId].productName = productName;
+        products[productId].productCode = productCode;
         products[productId].manufacturer = msg.sender;
         products[productId].productCount = productCount;
+        products[productId].currentOwner = msg.sender;
+        products[productId].productState = productState.Neutral;
+        participants[participantId].role = Roles.Active;
+
+        emit ProductCreated(msg.sender, productCost, productCode, productCount, productId);
+
         return productId;
     }
 
-    function updateProductCount(uint256 productId, uint256 productCount)
-        public
+    function updateProduct(uint256 productId, uint256 productCount, uint256 productCost, uint256 productCode)
+        public onlyRegisteredParticipant()
         returns (bool)
     {
-        require(products[productId].manufacturer == msg.sender);
+        require(products[productId].currentOwner == msg.sender);
         products[productId].productCount = safeAdd(products[productId].productCount,productCount);
+        products[productId].productCost = safeAdd(products[productId].productCost,productCost);
+        emit ProductUpdated(msg.sender, productCost, productCount, productCode, productId);
         return true;
     }
 
-    function getProduct(uint256 productId) public view returns (string memory,uint256,address,uint256) {
+    function getProduct(uint256 productId) public view onlyRegisteredParticipant() returns (uint256 memory,uint256,address,uint256) {
         return (
-            products[productId].productName,            
+            products[productId].productCode,            
             products[productId].productCost,
             products[productId].manufacturer,
+            products[productId].currentOwner,
             products[productId].productCount
         );
     }
 
-    struct participant {
-        string userName;
-        address addr;
-        uint256 role;
+    function registerParticipant(uint256 memory userCodeName, Roles role) public returns (uint256) {
+        participantCounter++;
+        uint256 participantId = participantCounter;
+        participants[participantId].userCodeName = userCodeName;
+        participants[participantId].addr = msg.sender;
+        participants[participantId].role = role;
+        participants[participantId].isRegistered = true;
+        membership[msg.sender] = 1;
+        return participantId;
     }
 
-    mapping(uint256 => participant) public participants;
-    event ProductCreated(address indexed from, address indexed to, uint tokens);
-    event ProductUpdated(address indexed tokenOwner, address indexed spender, uint tokens);
+    function request(address buyer, uint256 productId, uint256 hash, uint256 participantId) onlyRegisteredParticipant public{
+        products[productId].status=productState.Requested;
+        products[productId].buyer=msg.sender;
+        products[productId].hashOfDetails = hash; 
+        participants[participantId].role = Roles.Active;
+    }
+
+
+    function response(uint256 productId, productState state, uint256 hash, uint256 productCount, uint256 productCost, uint256 productCode) onlyRegisteredParticipant public{
+        require(products[productId].currentOwner == msg.sender);
+        if (products[productId].hashOfDetails == hash){
+            products[productId].status=state;
+            products[productId].currentOwner=products[productId].buyer;
+            emit ProductSold(products[productId].buyer, productCost, productCount, productCode, productId, hash, state);
+        }
+        else{
+            revert();
+        }
+    }
+
+    function addProductReview(uint256 productId, uint256 parameter_1_rating, uint256 parameter_2_rating, uint256 parameter_3_rating, uint256 overallRating, uint256 productCount, uint256 productCost, uint256 productCode) onlyRegisteredParticipant public{
+        require(products[productId].currentOwner == msg.sender);
+        productReviewCounter++;
+        productReviews[productId].reviewer = msg.sender;
+        productReviews[productId].parameter_1_rating = parameter_1_rating;
+        productReviews[productId].parameter_2_rating = parameter_2_rating;
+        productReviews[productId].parameter_3_rating = parameter_3_rating;
+        productReviews[productId].overallRating = overallRating;
+        productReviews[productId].productReviewNumber = productReviewCounter;
+        emit ProductReviewUpdated(msg.sender, parameter_1_rating, parameter_2_rating, parameter_3_rating, overallRating, productReviewCounter, productCost, productCount, productCode, productId);
+    }
+
+
+    event ProductCreated(address owner, uint256 productCost, uint256 productCode, uint256 productCount, uint256 productId);
+    event ProductUpdated(address currentOwner, uint256 productCost, uint256 productCount, uint256 productCode, uint256 productId);
+    event ProductSold(address currentOwner, uint256 productCost, uint256 productCount, uint256 productCode, uint256 productId, uint256 hashOfDetails, productState productStatus);
+    event ProductReviewUpdated(address productReviewer, uint256 parameter_1_rating, uint256 parameter_2_rating, uint256 parameter_3_rating, uint256 overallRating, uint256 productReviewCounter, uint256 productCost, uint256 productCount, uint256 productCode, uint256 productId);
 
 }
 
